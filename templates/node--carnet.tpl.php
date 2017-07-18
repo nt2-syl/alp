38d2f82f87d6461caa70496366a9b848b9dab5ab
taclab <mehdi.bourgeois@gmail.Com>
2017-07-18 13:52:33 +0200

    Ajout du field image encore

--- templates/node--carnet.tpl.php
+++ templates/node--carnet.tpl.php
@@ -80,11 +80,12 @@
  */
 ?>
 
-<?php $node->getField('field_image_fond'); ?>
+
 
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php if ((!$page && !empty($title)) || !empty($title_prefix) || !empty($title_suffix) || $display_submitted): ?>
   <header>
+  <?php print render($content['field_image_fond']); ?>
     <?php print render($title_prefix); ?>
     <?php if (!$page && !empty($title)): ?>
     <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
