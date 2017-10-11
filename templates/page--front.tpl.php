93483dd6499866ffa626cae43861e6dbcc677662
taclab <mehdi.bourgeois@gmail.Com>
2017-10-11 12:24:39 +0200

    Ajout du footer en full width

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -179,7 +179,9 @@
 </div>
 
 <?php if (!empty($page['footer'])): ?>
-  <footer class="footer <?php print $container_class; ?>">
+  <footer class="footer">
+    <div class="container">
     <?php print render($page['footer']); ?>
+    </div>
   </footer>
 <?php endif; ?>
